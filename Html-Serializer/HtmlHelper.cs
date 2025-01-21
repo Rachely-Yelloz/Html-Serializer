07ddc022172c318cc4671ffb085e1a9030289a2a
Rachel Yelloz <rsy262618@gmail.com>
2025-01-22 00:48:09 +0200

    start

---
+++ Html-Serializer/HtmlHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Html_Serializer
+{
+    public class HtmlHelper
+    {
+        private static HtmlHelper _instance= new HtmlHelper();
+        public static HtmlHelper Instance=> _instance;
+        public string[] HtmlTags { get; /*set;*/ }
+        public string[] HtmlVoidTags { get;/* set;*/ }
+
+        private HtmlHelper()
+        {
+            // קריאת תוכן הקובץ כטקסט
+            var htmlTagsJson = File.ReadAllText("JSON-Files/HtmlTags.json");
+            var htmlVoidTagsJson = File.ReadAllText("JSON-Files/HtmlVoidTags.json");
+
+            // המרת תוכן JSON למערך באמצעות JsonSerializer
+            HtmlTags = JsonSerializer.Deserialize<string[]>(htmlTagsJson);
+            HtmlVoidTags = JsonSerializer.Deserialize<string[]>(htmlVoidTagsJson);
+        }
+
+    }
+}
--- Html-Serializer/Program.cs
+++ Html-Serializer/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Html_Serializer;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+static async Task<string> Load(string url)
+{
+    HttpClient client = new HttpClient();
+    var response = await client.GetAsync(url);
+    var html = await response.Content.ReadAsStringAsync();
+    return html;
+}
+var html=await Load("https://www.malkabruk.co.il/");
+var cleanHtml = new Regex("//s").Replace(html, "");
+var htmlLines = new Regex("<(.*?)>").Split(cleanHtml).Where(s => s.Length > 0).ToArray();
+
+var htmlElement = "<div id=\"my-id\" class=\"my-class-1 my-class-2\" width=\"100%\">text</div>";
+var attributes = new Regex("([^\\s]*?)=\"(.*?)\"").Matches(htmlElement);
+HtmlElement root = new HtmlElement();
+HtmlElement current = root;
+
+for (int i = 0; i < htmlLines.Length; i++)
+{
+    int spaceIndex = htmlLines[i].IndexOf(' ');
+    if(spaceIndex != -1)
+    {
+        string n= htmlLines[i].Substring(0, spaceIndex);
+        if(n== "html/")
+        {
+            //הגעתי לסוף HTML
+        }
+        else if (n.StartsWith('/'))
+        {
+            //סגירת תווית
+            current = current.Parent;
+        }
+        else if (HtmlHelper.Instance.HtmlTags.Any(tags => tags == n)
+            || HtmlHelper.Instance.HtmlVoidTags.Any(tags => tags == n))
+        {
+            HtmlElement newChild = new HtmlElement();
+            //ביטוי רגולרי להכנסה של אטרביוט ועוד
+            current.Children.Add(newChild);
+        }
+    }
+
+}
